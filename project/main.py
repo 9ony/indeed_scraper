7bcdc1fe6769bfa4a5c0c43aaaa8c9555199d2be
9ony <darsta7@gmail.com>
2022-01-09 20:53:03 +0900

    version3

--- project/main.py
+++ project/main.py
@@ -1,16 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
-
-indeed_results = requests.get("https://www.indeed.com/jobs?q=python&limit=50&radius=25&start=0")
+s_num = 0 
+l_num = 50
+indeed_results = requests.get(f"https://www.indeed.com/jobs?q=python&limit={l_num}&radius=25&start={s_num}")
 indeed_soup = BeautifulSoup(indeed_results.text , 'html.parser')
 
 pagination = indeed_soup.find("ul",{"class": "pagination-list"})
-
+CountPage = indeed_soup.find("div",{"class": "SearchCountPages"})
 
 pages = pagination.find_all('li')
 aria_labels = []
 for page in pages:
-    if None!=page.find("b"):
-        aria_labels.append(page.find("b"))
+    if page.find("span",{"class":"np"})==None and page.find("b")==None :
+        aria_labels.append(page.find("span"))
 
 print(aria_labels)
