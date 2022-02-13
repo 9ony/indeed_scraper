3cf41b7526f4246e32baee739f6ac873ba7a7531
9ony <darsta7@gmail.com>
2022-02-13 19:17:42 +0900

    Indeed Scrapper 3.0 (report값 특수문자 및 공백 처리)

--- project/app.py
+++ project/app.py
@@ -1,4 +1,4 @@
-from flask import redirect, render_template , Flask , request
+from flask import redirect, render_template , Flask , request , Response , send_file
 from indeed import pages_max_func
 from indeed import indeed_jobs
 from save import save_file
@@ -15,14 +15,18 @@ def index():
 
 @app.route('/report')
 def report():
+    
     word = request.args.get('word')
-    if word:
+    #word가 공백이거나 특수문자로만 이루어져있을때 word에 null을 할당
+    if ''.join(filter(str.isalnum,word))=="" or word.strip(' ')=="":
+        word = "null"
+    if word != "null":
         word = word.lower()
+        word = ''.join(filter(str.isalnum,word))
         URL = f"/취업?q={word}&l=서울&limit=50&radius=100&sort=date"
         save_file(0,word)
         max_page = pages_max_func(0,URL)
         jobs = indeed_jobs(max_page , URL , word)
-        print(type(jobs))
     else:
         return redirect('/')
     return render_template("report.html" ,
